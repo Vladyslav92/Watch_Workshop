18609f81b41dd53a3b233e6956a84bc109ee821c
Vladislav <vs5956012@gmail.com>
2024-12-26 21:13:03 +0300

    upload the project

---
+++ app.py
@@ -0,0 +1,92 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from dotenv import load_dotenv
+
+# To run the application, in the terminal we need to first activate the virtual environment
+# using the command .\venv\Scripts\activate. Then, after (venv) we see in the terminal,
+# write the (python app.py) command to run the application from the virtual environment.
+# To stop the server, press Ctrl + C.
+
+load_dotenv('config.txt')
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'instance/mydatabase.db')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+@app.route('/')
+def home():
+    return render_template('main.html')
+
+@app.route('/news')
+def news():
+    return render_template('news.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = generate_password_hash(request.form['password'])
+        new_user = User(username=username, email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            session['user_id'] = user.id
+            session['username'] = user.username
+            if username == 'Admin' and check_password_hash(user.password, password):
+                return redirect(url_for('admin'))
+            return redirect(url_for('user'))
+        else:
+            error = 'Invalid username or password'
+            return render_template('login.html', error=error)
+    return render_template('login.html')
+
+@app.route('/admin')
+def admin():
+    if 'username' in session and session['username'] == 'Admin':
+        users = User.query.all()
+        return render_template('admin.html', users=users)
+    return redirect(url_for('login'))
+
+@app.route('/user')
+def user():
+    if 'username' in session:
+        username = session['username']
+        return render_template('user.html', username=username)
+    return redirect(url_for('login'))
+
+@app.route('/delete_user/<int:user_id>', methods=['POST'])
+def delete_user(user_id):
+    user = User.query.get(user_id)
+    if user and user.username != 'Admin':
+        db.session.delete(user)
+        db.session.commit()
+    return redirect(url_for('admin'))
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
---
+++ static/scripts/window.js
@@ -0,0 +1,7 @@
+document.getElementById("openButton").onclick = function () {
+  document.getElementById("popupWindow").style.display = "block";
+};
+
+document.getElementById("closeButton").onclick = function () {
+  document.getElementById("popupWindow").style.display = "none";
+};
